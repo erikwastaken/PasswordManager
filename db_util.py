d8af2653fee07587835fecf25f42d0107cd80419
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2021-01-31 12:23:55 +0100

    moved execute SQL methods to db_util.py
    changed db_util.py from class to module to better capture the singleton aspect;

--- db_util.py
+++ db_util.py
@@ -1,15 +1,70 @@
 from configparser import ConfigParser
+import psycopg2
 
-class DbUtil:
-  def read_db_config(filename='database.ini',section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-      params = parser.items(section)
-      for param in params:
-        db[param[0]] = param[1]
-    else:
-      raise Exception('Section {0} not found in file {1}'.format(section,filename))
-    return db
+
+def read_db_config(filename='database.ini',section='postgresql'):
+  parser = ConfigParser()
+  parser.read(filename)
+  # get section, default to postgresql
+  db = {}
+  if parser.has_section(section):
+    contents = parser.items(section)
+    for param in contents:
+      db[param[0]] = param[1]
+  else:
+    raise Exception('Section {0} not found in file {1}'.format(section,filename))
+  return db
+
+params = read_db_config()
+
+def execute_statement_and_commit(sql_statement):
+  conn = None
+  try:
+    conn = psycopg2.connect(**params)
+    # create a cursor
+    cur = conn.cursor()
+    cur.execute(sql_statement)
+    conn.commit()
+    # close the communication with PostgreSQL
+    cur.close()
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+  finally:
+    if conn is not None:
+      conn.close()
+
+def execute_select_single(sql_statement):
+  conn = None
+  result = None
+  try:
+    conn = psycopg2.connect(**params)
+    # create a cursor
+    cur = conn.cursor()
+    cur.execute(sql_statement)
+    result = cur.fetchone()
+    # close the communication with PostgreSQL
+    cur.close()
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+  finally:
+    if conn is not None:
+      conn.close()
+  return result
+
+def execute_select_all(sql_statement):
+  conn = None
+  result = None
+  try:
+    conn = psycopg2.connect(**params)
+    # create a cursor
+    cur = conn.cursor()
+    cur.execute(sql_statement)
+    result = cur.fetchall()
+    # close the communication with PostgreSQL
+    cur.close()
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+  finally:
+    if conn is not None:
+      conn.close()
+  return result
--- user.py
+++ user.py
@@ -1,20 +1,16 @@
-import psycopg2
 import hashlib
-from db_util import DbUtil
+import db_util
 
 class User:
-  name = " "
-  password = " "
-  db_config = " "
-  
   def __init__(self, name, password):
     self.name = name
     self.password = password
-    self.params = DbUtil.read_db_config()
+    # self.params = db_util.read_db_config()
+    db_util.read_db_config()
   
   def is_authenticated(self):
     sql_statement = 'SELECT * FROM users WHERE user_id = \'{0}\';'.format(self.name) 
-    db_user = self.__execute_select_single(sql_statement)
+    db_user = db_util.execute_select_single(sql_statement)
     # is the password correct?
     hashed_password = hashlib.sha256(self.password.encode('utf-8')).hexdigest()
     return (hashed_password == db_user[1])
@@ -22,7 +18,7 @@ class User:
   # should return a list of hash maps, i.e. dictionaries
   def get_accounts(self):
     sql_statement = 'SELECT * FROM accounts WHERE user_id = \'{0}\';'.format(self.name)
-    db_accounts = self.__execute_select_all(sql_statement)
+    db_accounts = db_util.execute_select_all(sql_statement)
     res_accounts = []
     if db_accounts:
       for dba in db_accounts:
@@ -37,7 +33,7 @@ class User:
   def create_account(self,service, login_name, login_password):
     sql_statement = '''INSERT INTO accounts (user_id,service,login_name,login_password)
                        VALUES (\'{0}\',\'{1}\',\'{2}\',\'{3}\');'''.format(self.name,service,login_name,login_password) 
-    self.__execute_statement_and_commit(sql_statement)
+    db_util.execute_statement_and_commit(sql_statement)
     
   def change_account_password(self,new_password,service,login_name):
     sql_statement = '''UPDATE accounts
@@ -45,64 +41,12 @@ class User:
                        WHERE user_id = \'{1}\'
                          AND service = \'{2}\'
                          AND login_name = \'{3}\';'''.format(new_password,self.name,service,login_name)
-    self.__execute_statement_and_commit(sql_statement)
-
-  def __execute_statement_and_commit(self,sql_statement):
-    conn = None
-    try:
-      conn = psycopg2.connect(**self.params)
-      # create a cursor
-      cur = conn.cursor()
-      cur.execute(sql_statement)
-      conn.commit()
-      # close the communication with PostgreSQL
-      cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-      print(error)
-    finally:
-      if conn is not None:
-        conn.close()
-
-  def __execute_select_single(self,sql_statement):
-    conn = None
-    result = None
-    try:
-      conn = psycopg2.connect(**self.params)
-      # create a cursor
-      cur = conn.cursor()
-      cur.execute(sql_statement)
-      result = cur.fetchone()
-      # close the communication with PostgreSQL
-      cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-      print(error)
-    finally:
-      if conn is not None:
-        conn.close()
-    return result
-
-  def __execute_select_all(self,sql_statement):
-    conn = None
-    result = None
-    try:
-      conn = psycopg2.connect(**self.params)
-      # create a cursor
-      cur = conn.cursor()
-      cur.execute(sql_statement)
-      result = cur.fetchall()
-      # close the communication with PostgreSQL
-      cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-      print(error)
-    finally:
-      if conn is not None:
-        conn.close()
-    return result
+    db_util.execute_statement_and_commit(sql_statement)
 
   def delete_account(self,service,login_name):
     sql_statement = '''DELETE FROM accounts 
                         WHERE user_id = \'{0}\'
                           AND service = \'{1}\'
                           AND login_name = \'{2}\';'''.format(self.name,service,login_name)
-    self.__execute_statement_and_commit(sql_statement)
+    db_util.execute_statement_and_commit(sql_statement)
 
