637a9e3717bc38e77bc7f6467557e4d4b4ed7b26
erikwastaken <46598911+erikwastaken@users.noreply.github.com>
2021-02-01 20:13:17 +0100

    moved menu options into their own functions

--- menu.py
+++ menu.py
@@ -25,44 +25,13 @@ class Menu:
       answer = input("choose: ")
     print()
     if answer == '1':
-      service = input("Service: ")
-      login_name = input("Login Name: ")
-      login_password = input("Password: ")
-      self.user.create_account(service,login_name,login_password)
+      self.__create_new_account()
     elif answer == '2':
-      self.__display_accounts()
-      print()
-      index = input("Choose password: ")
-      print()
-      print("1 - display password")
-      print("2 - copy password to clipboard")
-      i = input("Choose: ")
-      if i == '1':
-        print(self.user.get_accounts()[int(index)]['login_password'])
-      elif i == '2':
-        pyperclip.copy(self.user.get_accounts()[int(index)]['login_password'])
-        print("Password copied to clipboard")
-      else:
-        print("wrong input")
+      self.__get_password()
     elif answer == '3':
-      self.__display_accounts()
-      print()
-      index = int(input("Which password should be updated? "))
-      new_password = input("Enter new password: ")
-      service = self.user.get_accounts()[index]['service']
-      login_name = self.user.get_accounts()[index]['login_name']
-      self.user.change_account_password(new_password,service,login_name)
-      print("Password for {0} changed to {1}".format(service,new_password))
+      self.__change_password() 
     elif answer == '4':
-      self.__display_accounts()
-      print()
-      index = int(input("Which password should be deleted? "))
-      service = self.user.get_accounts()[index]['service']
-      login_name = self.user.get_accounts()[index]['login_name']
-      confirmation_answer = input("Are you sure that you want to delete the account for service {0}? Y/N ".format(service))
-      if confirmation_answer.upper() == 'Y':
-        self.user.delete_account(service,login_name)
-        print("Account for service {0} has been deleted.".format(service))
+      self.__delete_login()
     elif answer.upper() == 'Q':
       return
 
@@ -98,3 +67,45 @@ class Menu:
       print('{0} | {1} | {2}'.format(c,a['service'], a['login_name']))
       c += 1
 
+  def __create_new_account(self):
+    service = input("Service: ")
+    login_name = input("Login Name: ")
+    login_password = input("Password: ")
+    self.user.create_account(service,login_name,login_password)
+
+  def __get_password(self):
+    self.__display_accounts()
+    print()
+    index = input("Choose password: ")
+    print()
+    print("1 - display password")
+    print("2 - copy password to clipboard")
+    i = input("Choose: ")
+    if i == '1':
+      print(self.user.get_accounts()[int(index)]['login_password'])
+    elif i == '2':
+      pyperclip.copy(self.user.get_accounts()[int(index)]['login_password'])
+      print("Password copied to clipboard")
+    else:
+      print("wrong input")
+  
+  def __change_password(self):
+    self.__display_accounts()
+    print()
+    index = int(input("Which password should be updated? "))
+    new_password = input("Enter new password: ")
+    service = self.user.get_accounts()[index]['service']
+    login_name = self.user.get_accounts()[index]['login_name']
+    self.user.change_account_password(new_password,service,login_name)
+    print("Password for {0} changed to {1}".format(service,new_password))
+
+  def __delete_login(self):
+    self.__display_accounts()
+    print()
+    index = int(input("Which password should be deleted? "))
+    service = self.user.get_accounts()[index]['service']
+    login_name = self.user.get_accounts()[index]['login_name']
+    confirmation_answer = input("Are you sure that you want to delete the account for service {0}? Y/N ".format(service))
+    if confirmation_answer.upper() == 'Y':
+      self.user.delete_account(service,login_name)
+      print("Account for service {0} has been deleted.".format(service))
